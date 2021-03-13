31a538aaa15b7b0c96fccad57375727548c6272e
caozhiyi <caozhiyi5@gmail.com>
2021-03-13 10:34:45 +0800

    add ack frame.

---
+++ src/quic/frame/ack_frame.cpp
@@ -0,0 +1,144 @@
+#include "ack_frame.h"
+#include "common/decode/normal_decode.h"
+#include "common/buffer/buffer_interface.h"
+#include "common/alloter/alloter_interface.h"
+
+namespace quicx {
+
+AckFrame::AckFrame():
+    Frame(FT_ACK),
+    _largest_ack(0),
+    _ack_delay(0),
+    _first_ack_range(0) {
+
+}
+
+AckFrame::~AckFrame() {
+
+}
+
+bool AckFrame::Encode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter) {
+    uint16_t size = EncodeSize();
+
+    char* data = alloter->PoolMalloc<char>(size);
+
+    char* pos = EncodeFixed<uint16_t>(data, _frame_type);
+    pos = EncodeVarint(pos, _largest_ack);
+    pos = EncodeVarint(pos, _ack_delay);
+    pos = EncodeVarint(pos, _ack_ranges.size());
+    pos = EncodeVarint(pos, _first_ack_range);
+
+    for (size_t i = 0; i < _ack_ranges.size(); i++) {
+        pos = EncodeVarint(pos, _ack_ranges[i]._gap);
+        pos = EncodeVarint(pos, _ack_ranges[i]._ack_range);
+    }
+    
+    buffer->Write(data, pos - data);
+    alloter->PoolFree(data, size);
+    return true;
+}
+
+bool AckFrame::Decode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter, bool with_type) {
+    uint16_t size = EncodeSize();
+
+    char* data = alloter->PoolMalloc<char>(size);
+    buffer->ReadNotMovePt(data, size);
+    uint32_t ack_range_count = 0;
+
+    char* pos = data;
+    if (with_type) {
+        pos = DecodeFixed<uint16_t>(data, data + size, _frame_type);
+    }
+    pos = DecodeVirint(pos, data + size, _largest_ack);
+    pos = DecodeVirint(pos, data + size, _ack_delay);
+    pos = DecodeVirint(pos, data + size, ack_range_count);
+    pos = DecodeVirint(pos, data + size, _first_ack_range);
+
+    buffer->MoveReadPt(pos - data);
+    alloter->PoolFree(data, size);
+
+    size = ack_range_count * sizeof(AckRange);
+    data = alloter->PoolMalloc<char>(size);
+    buffer->Read(data, size);
+    pos = data;
+
+    uint32_t gap;
+    uint32_t range;
+    for (uint32_t i = 0; i < ack_range_count; i++) {
+        pos = DecodeVirint(pos, data + size, gap);
+        pos = DecodeVirint(pos, data + size, range);
+        _ack_ranges.emplace_back(AckRange{gap, range});
+    }
+    buffer->MoveReadPt(pos - data);
+    alloter->PoolFree(data, size);
+    
+    return true;
+}
+
+uint32_t AckFrame::EncodeSize() {
+    return sizeof(AckFrame) + _ack_ranges.size() * sizeof(AckRange);
+}
+
+AckFrame::AckFrame(FrameType ft):
+    Frame(ft),
+    _largest_ack(0),
+    _ack_delay(0),
+    _first_ack_range(0) {
+
+}
+
+
+AckEcnFrame::AckEcnFrame():
+    AckFrame(FT_ACK_ECN),
+    _ect_0(0),
+    _ect_1(0),
+    _ecn_ce(0) {
+
+}
+
+AckEcnFrame::~AckEcnFrame() {
+
+}
+
+bool AckEcnFrame::AckEcnFrame::Encode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter) {
+    AckFrame::Encode(buffer, alloter);
+
+    uint16_t size = EncodeSize();
+
+    char* data = alloter->PoolMalloc<char>(size);
+
+    char* pos = EncodeFixed<uint16_t>(data, _frame_type);
+    pos = EncodeVarint(pos, _ect_0);
+    pos = EncodeVarint(pos, _ect_1);
+    pos = EncodeVarint(pos, _ecn_ce);
+
+    buffer->Write(data, pos - data);
+    alloter->PoolFree(data, size);
+
+    return true;
+}
+
+bool AckEcnFrame::AckEcnFrame::Decode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter, bool with_type) {
+    AckFrame::Decode(buffer, alloter, with_type);
+
+    uint16_t size = EncodeSize();
+
+    char* data = alloter->PoolMalloc<char>(size);
+    buffer->ReadNotMovePt(data, size);
+    char* pos = data;
+
+    pos = DecodeVirint(pos, data + size, _ect_0);
+    pos = DecodeVirint(pos, data + size, _ect_1);
+    pos = DecodeVirint(pos, data + size, _ecn_ce);
+
+    buffer->MoveReadPt(pos - data);
+    alloter->PoolFree(data, size);
+
+    return true;
+}
+
+uint32_t AckEcnFrame::AckEcnFrame::EncodeSize() {
+    return sizeof(AckEcnFrame) - sizeof(AckFrame);
+}
+
+}
--- src/quic/frame/ack_frame.h
+++ src/quic/frame/ack_frame.h
@@ -7,37 +7,73 @@
 
 namespace quicx {
 
-class AckFrame : public Frame {
+struct AckRange {
+    uint32_t _gap;         // the number of contiguous unacknowledged packets preceding the packet number one lower than the smallest in the preceding ACK Range.
+    uint32_t _ack_range;   // the number of contiguous acknowledged packets preceding the largest packet number.
+};
+
+class AckFrame: public Frame {
 public:
-    AckFrame() : Frame(FT_ACK) {}
-    ~AckFrame() {}
+    AckFrame();
+    ~AckFrame();
+
+    bool Encode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter);
+    bool Decode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter, bool with_type = false);
+    uint32_t EncodeSize();
+
+    void SetLargestAck(uint64_t ack) { _largest_ack = ack; }
+    uint64_t GetLargestAck() { return _largest_ack; }
+
+    void SetAckDelay(uint32_t delay) { _ack_delay = delay; }
+    uint32_t GetAckDelay() { return _ack_delay; }
+
+    void SetFirstAckRange(uint32_t range) { _first_ack_range = range; }
+    uint32_t GetFirstAckRange() { return _first_ack_range; }
+
+    void AddAckRange(uint32_t gap, uint32_t range) { _ack_ranges.emplace_back(AckRange{gap, range}); }
+    const std::vector<AckRange>& GetAckRange() { return _ack_ranges; }
 
 protected:
-    AckFrame(FrameType ft) : Frame(ft) {}
+    AckFrame(FrameType ft);
 
 private:
-    struct AckRange {
-        uint32_t _gap;         // the number of contiguous unacknowledged packets preceding the packet number one lower than the smallest in the preceding ACK Range.
-        uint32_t _ack_range;   // the number of contiguous acknowledged packets preceding the largest packet number.
-    };
-    uint32_t _largest_ack;     // largest packet number the peer is acknowledging.
+    uint64_t _largest_ack;     // largest packet number the peer is acknowledging.
     uint32_t _ack_delay;       // the time delta in microseconds between when this ACK was sent and when the largest acknowledged packet.
+    /*
     uint32_t _ack_range_count; // the number of Gap and ACK Range fields in the frame.
+    AckRange *_ack_ranges      // ranges of packets which are alternately not acknowledged (Gap) and acknowledged (ACK Range).
+    */
     uint32_t _first_ack_range; // the number of contiguous packets preceding the Largest Acknowledged that are being acknowledged.
-    std::vector<AckRange> _ack_ranges; // ranges of packets which are alternately not acknowledged (Gap) and acknowledged (ACK Range).
+    std::vector<AckRange> _ack_ranges;
+
+    /*0    1    2    3    4    5    6    7    8    9    10    11    12    13    14     15    16    17    18    19   20 */
+    /*          <range:3  gap:4>         <range:4          gap:4>               <first_ack_range:6      largest_ack:20>*/                   
 };
 
-class AckEcnFrame : public AckFrame {
+
+class AckEcnFrame: public AckFrame {
 public:
-    AckEcnFrame() : AckFrame(FT_ACK_ECN) {}
-    ~AckEcnFrame() {}
+    AckEcnFrame();
+    ~AckEcnFrame();
+
+    bool Encode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter);
+    bool Decode(std::shared_ptr<Buffer> buffer, std::shared_ptr<AlloterWrap> alloter, bool with_type = false);
+    uint32_t EncodeSize();
+
+    void SetEct0(uint64_t ect0) { _ect_0 = ect0; }
+    uint64_t GetEct0() { return _ect_0; }
+
+    void SetEct1(uint64_t ect1) { _ect_1 = ect1; }
+    uint64_t GetEct1() { return _ect_1; }
+
+    void SetEcnCe(uint64_t ce) { _ecn_ce = ce; }
+    uint64_t GetEcnCe() { return _ecn_ce; }
 
 private:
-    uint32_t _ect_0; // the total number of packets received with the ECT(0) codepoint in the packet number space of the ACK frame.
-    uint32_t _ect_1; // the total number of packets received with the ECT(1) codepoint in the packet number space of the ACK frame.
-    uint32_t _ect;   // the total number of packets received with the CE codepoint in the packet number space of the ACK frame.
+    uint64_t _ect_0;    // the total number of packets received with the ECT(0) codepoint in the packet number space of the ACK frame.
+    uint64_t _ect_1;    // the total number of packets received with the ECT(1) codepoint in the packet number space of the ACK frame.
+    uint64_t _ecn_ce;   // the total number of packets received with the CE codepoint in the packet number space of the ACK frame.
 };
-
 
 }
 
