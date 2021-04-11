81dd58c03e6849f0efdbdf7e99512f892d1cb91f
caozhiyi <caozhiyi5@gmail.com>
2021-04-11 19:53:17 +0800

    add connection interface.

--- src/quic/connection/connection_interface.h
+++ src/quic/connection/connection_interface.h
@@ -7,12 +7,17 @@
 namespace quicx {
 
 class Frame;
+class Address;
 class Connection {
 public:
     Connection() {}
     virtual ~Connection() {}
 
+    virtual bool Open(Address& addr, uint32_t strean_limit) = 0;
+
     virtual void Send(std::shared_ptr<Frame> frame) = 0;
+
+    virtual void Close() = 0;
 private:
     uint64_t _connection_id;
 };
