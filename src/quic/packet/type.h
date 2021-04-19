f526131b186382ffee86ab51334f5c58b4993e9b
caozhiyi <272653256@qq.com>
2021-04-19 18:43:34 +0800

    add packet type.

--- src/quic/packet/type.h
+++ src/quic/packet/type.h
@@ -17,7 +17,25 @@ enum PacketHeaderType {
     PHT_SHORT   = 0x00,
 };
 
-static const unsigned int16_t __fix_header_btye = 0x04;
+static const uint16_t __fix_header_btye = 0x04;
+
+enum PacketHeaderFormat: uint8_t {
+  PHF_IETF_QUIC_LONG_HEADER_PACKET  = 0,
+  PHF_IETF_QUIC_SHORT_HEADER_PACKET = 1,
+  PHF_GOOGLE_QUIC_PACKET            = 2,
+};
+
+enum QuicLongHeaderType: uint8_t {
+  QLHT_VERSION_NEGOTIATION = 0,
+  QLHT_INITIAL             = 1,
+  QLHT_ZERO_RTT_PROTECTED  = 2,
+  QLHT_HANDSHAKE           = 3,
+  QLHT_RETRY               = 4,
+
+  QLHT_INVALID_PACKET_TYPE = 5,
+};
+
+
 
 }
 
