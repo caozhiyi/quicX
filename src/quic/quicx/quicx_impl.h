af1c1c60142ac3a689e63d33afdcad9d98fadc3a
caozhiyi <caozhiyi5@gmail.com>
2024-11-28 17:59:31 +0800

    redesign quic interface.

--- src/quic/quicx/quicx_impl.h
+++ src/quic/quicx/quicx_impl.h
@@ -1,35 +1,35 @@
-#ifndef QUIC_QUICX_QUICX_IMPL
-#define QUIC_QUICX_QUICX_IMPL
+// #ifndef QUIC_QUICX_QUICX_IMPL
+// #define QUIC_QUICX_QUICX_IMPL
 
-#include <memory>
-#include <vector>
-#include "quic/include/quicx.h"
-#include "common/thread/thread.h"
-#include "quic/crypto/tls/tls_ctx.h"
-#include "quic/quicx/receiver_interface.h"
-#include "quic/quicx/processor_interface.h"
+// #include <memory>
+// #include <vector>
+// #include "quic/include/quicx.h"
+// #include "common/thread/thread.h"
+// #include "quic/crypto/tls/tls_ctx.h"
+// #include "quic/quicx/receiver_interface.h"
+// #include "quic/quicx/processor_interface.h"
 
-namespace quicx {
-namespace quic {
+// namespace quicx {
+// namespace quic {
 
-class QuicxImpl:
-    public Quicx {
-public:
-    QuicxImpl();
-    virtual ~QuicxImpl();
+// class QuicxImpl:
+//     public Quicx {
+// public:
+//     QuicxImpl();
+//     virtual ~QuicxImpl();
 
-    virtual bool Init(uint16_t thread_num);
-    virtual void Join();
-    virtual void Destroy();
-    virtual bool Connection(const std::string& ip, uint16_t port);
-    virtual bool ListenAndAccept(const std::string& ip, uint16_t port);
-private:
-    std::shared_ptr<TLSCtx> _ctx;
-    std::shared_ptr<IReceiver> _receiver;
-    std::vector<std::shared_ptr<IProcessor>> _processors;
-};
+//     virtual bool Init(uint16_t thread_num);
+//     virtual void Join();
+//     virtual void Destroy();
+//     virtual bool Connection(const std::string& ip, uint16_t port);
+//     virtual bool ListenAndAccept(const std::string& ip, uint16_t port);
+// private:
+//     std::shared_ptr<TLSCtx> _ctx;
+//     std::shared_ptr<IReceiver> _receiver;
+//     std::vector<std::shared_ptr<IProcessor>> _processors;
+// };
 
-}
-}
+// }
+// }
 
-#endif
+// #endif
