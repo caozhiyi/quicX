fc3d37d4b3b9b1f22fa359ea0399bba2e5bf843b
caozhiyi <272653256@qq.com>
2025-02-10 22:25:31 +0800

    add server push example.

---
+++ example/server_push/client.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include "http3/include/if_client.h"
+
+
+int main() {
+    auto client = quicx::http3::IClient::Create();
+
+    client->Init(1, quicx::http3::LogLevel::kDebug);
+
+    auto request = quicx::http3::IRequest::Create();
+    request->SetBody("hello world");
+    client->DoRequest(
+        "https://127.0.0.1:8882/hello",
+        quicx::http3::HttpMethod::kGet,
+        request, 
+        [](std::shared_ptr<quicx::http3::IResponse> response, uint32_t error) {
+            std::cout << "status: " << response->GetStatusCode() << std::endl;
+            std::cout << "response: " << response->GetBody() << std::endl;
+        }
+    );
+    client->SetPushPromiseHandler(
+        [](std::unordered_map<std::string, std::string>& headers)->bool{
+            for (auto iter : headers) {
+                std::cout << "get push promise. header:" << iter.first << "value:" << iter.second << std::endl;
+            }
+            return true;
+        }
+    );
+
+    client->SetPushHandler(
+        [](std::shared_ptr<quicx::http3::IResponse> response, uint32_t error) {
+            std::cout << "push status: " << response->GetStatusCode() << std::endl;
+            std::cout << "push response: " << response->GetBody() << std::endl;
+        }
+    );
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
