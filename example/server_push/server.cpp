b89df2bcc35204eaef79812c3fe006b5609e92d9
caozhiyi <272653256@qq.com>
2025-03-02 18:29:56 +0800

    fix stream frame process failed issue.

--- example/server_push/server.cpp
+++ example/server_push/server.cpp
@@ -58,7 +58,7 @@ static const char key_pem[] =
             resp->AppendPush(push_resp);
         }
     );
-    server->Init(cert_pem, key_pem, 1, quicx::http3::LogLevel::kDebug);
+    server->Init(cert_pem, key_pem, 1, quicx::http3::LogLevel::kError);
     server->Start("0.0.0.0", 8882);
     server->Join();
 }
